c9e3eab31bdbe329cdea857c927bb01e5d7c0422
BryWillow <168703221+BryWillow@users.noreply.github.com>
2024-12-05 10:17:29 -0600

    Remove unnecessary files.

--- src/NeuWillow.cpp
+++ src/NeuWillow.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
-#include <string>
-#include <memory>
 #include "../include/Configuration.hpp"
 #include "../include/Simulation.hpp"
 
-void usage()
+std::string checkUsageOrExit(int argc, char* argv[])
 {
-  std::cout << std::endl;
-  std::cout << "Usage: NeuWillow [full_path_to_config_file]"  << std::endl;
-  std::cout << " E.g.: ./NeuWillow ../configs/myconfig.ini"   << std::endl;
-  std::cout << std::endl;
+  constexpr int expectedParamCount = 2;
+  if (argc != 2)
+  {
+    std::cout << std::endl << "Usage: NeuWillow [Full Path to Config File]" << std::endl;
+    std::cout << " E.g.: ./NeuWillow ../configs/myconfig.ini"   << std::endl << std::endl;
+    std::cout << "Error: Expected " << expectedParamCount << " parameters, but received " << argc << std::endl;
+    std::cout << std:: endl;
+    exit(0);
+  }
+
+  std::string configFile = argv[1];
+  return configFile;
 }
 
 int main(int argc, char* argv[])
 {
-  if (argc < 2)
-  {
-    usage();
-    return 0;
-  }
-
-  neuWillow::Simulation::start(argv[1]);
-
-  return 0;
+  std::string configFile = checkUsageOrExit(argc, argv);
+  neuWillow::Simulation::start(configFile);
 }
