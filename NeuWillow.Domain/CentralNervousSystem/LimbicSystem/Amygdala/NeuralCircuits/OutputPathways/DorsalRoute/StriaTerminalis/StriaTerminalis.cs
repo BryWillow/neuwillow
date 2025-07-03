a0a2cfe1ddb01b7b6763e72c9e47c952c48975fe
BryWillow <168703221+BryWillow@users.noreply.github.com>
2025-07-03 14:15:33 -0500

    Add Amygdala Dorsal and Ventral Output Pathways.

---
+++ NeuWillow.Domain/CentralNervousSystem/LimbicSystem/Amygdala/NeuralCircuits/OutputPathways/DorsalRoute/StriaTerminalis/StriaTerminalis.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeuWillow.Domain.CentralNervousSystem.LimbicSystem.Amygdala.NeuralCircuits.OutputPathways.DorsalRoute.StriaTerminalis
+{
+    public class StriaTerminalis
+    {
+        
+    }
+}
