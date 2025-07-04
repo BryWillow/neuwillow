83bbc4a265635b2e9e95cb4676835aa7affb9b94
BryWillow <168703221+BryWillow@users.noreply.github.com>
2025-07-04 11:17:30 -0500

    Mapped out the Hippocampus.

---
+++ NeuWillow.Domain/CentralNervousSystem/LimbicSystem/Hippocampus/GyrusFasciolaris/FimbriaOfForninx/FimbrioDentateSulcus/DentateGyrus/BandOfGiacomini/Uncus/Uncus.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NeuWillow.Domain.CentralNervousSystem.LimbicSystem.Hippocampus.GyrusFasciolaris.DentateGyrus.BandOfGiacomini.Uncus
+{
+    public class Uncus
+    {
+        
+    }
+}
