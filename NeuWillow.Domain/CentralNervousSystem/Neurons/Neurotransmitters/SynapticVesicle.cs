05840e2a675bbf150955d2cb24aa683e4379b0e9
BryWillow <168703221+BryWillow@users.noreply.github.com>
2025-07-07 20:36:20 -0500

    Finish the simple Synaptic Vesicle class.

--- NeuWillow.Domain/CentralNervousSystem/Neurons/Neurotransmitters/SynapticVesicle.cs
+++ NeuWillow.Domain/CentralNervousSystem/Neurons/Neurotransmitters/SynapticVesicle.cs
@@ -1,5 +1,9 @@
 namespace NeuWillow.Domain.CentralNervousSystem.Neurons.Neurotransmitters;
 
+/// <summary>
+/// A "sac-like" structure that contains neurotransmitters to be released.
+/// The neurotransmitters inside each vesicle are the result of the preceding synapse.
+/// </summary>
 public class SynapticVesicle(IEnumerable<Neurotransmitter> neurotransmitters)
 {
     public IEnumerable<Neurotransmitter> Neurotransmitters => neurotransmitters;
