d0b436c62cb2c49cbae2932c7f8641352a6e5dca
BryWillow <168703221+BryWillow@users.noreply.github.com>
2025-07-11 13:45:54 -0500

    Add unit test project. Begin testing synaptic vesicle recycling.

---
+++ NeuWillow.Tests/SynapseTests.cs
@@ -0,0 +1,19 @@
+﻿using NeuWillow.Domain.CentralNervousSystem.Neurons.Dendrites.DendriticSpines;
+using NeuWillow.Domain.CentralNervousSystem.Neurons.Neurotransmitters;
+using NeuWillow.Domain.CentralNervousSystem.Neurons.Synapses;
+
+namespace NeuWillow.Tests;
+
+public class SynapseTests
+{
+    [Fact]
+    public void CreateSynapticVesicle()
+    {
+        List<Neurotransmitter> neurotransmitters = [Neurotransmitter.Gaba, Neurotransmitter.Glutamate];
+        SynapticVesicle vesicle = new SynapticVesicle(neurotransmitters);
+        
+        Dendrite dendrite = new Dendrite();
+
+        Assert.True(dendrite != null);
+    }
+}
